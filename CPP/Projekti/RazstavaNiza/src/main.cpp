b305547a00d0538cf63b1c02cabb35142010f24d
Nejc <ethoarx@gmail.com>
2019-10-14 13:44:07 +0200

    Oki konec za zdaj

--- CPP/Projekti/RazstavaNiza/src/main.cpp
+++ CPP/Projekti/RazstavaNiza/src/main.cpp
@@ -10,6 +10,10 @@ int main() {
 
 	std::getline(std::cin, input);
 
+	Utils::cPrint("Dolzina niza je: " + static_cast<char>(input.length()));
+	Utils::cPrint(std::string("Prva crka niza je: \"") + input.at(1) + "\"");
+	Utils::cPrint(std::string("Zadnja crka niza je: \"") + input.at(input.length() - 1) + "\"");
+
 	Utils::cPrint("Tiskam crke iz niza: \"" + input + "\"");
 	for (std::string::size_type l = 0; l < input.length(); l++)
 		Utils::cPrint(input[l]);
