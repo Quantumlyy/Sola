f18a0746fd3fd09bef6132972c32f5bad19f5ac1
Nejc <ethoarx@gmail.com>
2019-10-30 15:15:54 +0100

    se en projekt

---
+++ CPP/Projekti/Ceasar/src/Utils.h
@@ -0,0 +1,13 @@
+#ifndef CPP_CEASAR_UTILS_H
+#define CPP_CEASAR_UTILS_H
+
+#include <string>
+
+class Utils {
+public:
+	static void cPrint(std::string str);
+	static void cPrint(char charArray[]);
+	static void cPrint(char cha);
+};
+
+#endif CPP_CEASAR_UTILS_H // CPP_CEASAR_UTILS_H
---
+++ CPP/Projekti/Ceasar/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+#include "Utils.h"
+
+int main() {
+	std::string alphabet = "abcdefghijklmnopqrstuvwxyz ";
+	std::string input = "";
+	std::string setMoveFactor = "";
+	std::string output = "";
+
+	Utils::cPrint("Prosim vnesi stavek za sifriranje");
+	std::getline(std::cin, input);
+
+	Utils::cPrint("Prosim vnesi stevilo za premik");
+	std::getline(std::cin, setMoveFactor);
+
+	for (auto ch : input) {
+		std::size_t found = alphabet.find(ch);
+		int moveFactor = std::stoi(setMoveFactor);
+
+		while ((found + moveFactor) >= alphabet.length())
+			moveFactor = moveFactor - alphabet.length();
+
+		output = output + alphabet.at(moveFactor + found);
+	}
+
+	Utils::cPrint(output);
+
+	Utils::cPrint("Pritisni katerokoli tipko za nadeljevanje");
+	std::getchar();
+
+	return EXIT_SUCCESS;
+}
