8e2438dcfc1651228f9045e6141a905e0a5dfffd
QuantumlyTangled <ethoarx@gmail.com>
2020-08-08 00:06:47 +0200

    RazstavaNiza

--- CPP/Projekti/RazstavaNiza/src/main.cpp
+++ CPP/Projekti/RazstavaNiza/src/main.cpp
@@ -4,19 +4,21 @@
 #include "Utils.h"
 
 int main() {
-	std::string input = "";
+	std::string input;
 
 	Utils::cPrint("Prosim vnesi niz za analizo");
 
 	std::getline(std::cin, input);
 
-	Utils::cPrint("Dolzina niza je: " + static_cast<char>(input.length()));
+	Utils::cPrint(&"Dolzina niza je: " [ static_cast<char>(input.length()) ]);
 	Utils::cPrint(std::string("Prva crka niza je: \"") + input.at(1) + "\"");
 	Utils::cPrint(std::string("Zadnja crka niza je: \"") + input.at(input.length() - 1) + "\"");
 
 	Utils::cPrint("Tiskam crke iz niza: \"" + input + "\"");
-	for (std::string::size_type l = 0; l < input.length(); l++)
-		Utils::cPrint(input[l]);
+
+	for (char &l : input) {
+        Utils::cPrint(l);
+    }
 
 	return EXIT_SUCCESS;
 }
