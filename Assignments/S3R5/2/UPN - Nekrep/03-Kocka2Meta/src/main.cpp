9b73be991851f76f5e9e8b203d2cdecb3db8e54f
QuantumlyTangled <ethoarx@gmail.com>
2020-09-03 07:11:15 +0200

    feat: 2UPN03Kocka2Meta

---
+++ Assignments/S3R5/2/UPN - Nekrep/03-Kocka2Meta/src/main.cpp
@@ -0,0 +1,19 @@
+// 03. Vaja Nejc Drobnic - Kocka2Meta (2UPN03Kocka2Meta)
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+	std::system("chcp 65001");
+	std::system("CLS");
+
+	std::srand(time(nullptr));
+
+	printf("Kocka 1: %i\n", 1 + rand() % 6);
+        printf("Kocka 2: %i\n", 1 + rand() % 6);
+
+	return 0;
+}
