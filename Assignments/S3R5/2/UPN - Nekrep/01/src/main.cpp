e4c947209818b4b4b5a6784dc8e7399313899cf2
QuantumlyTangled <ethoarx@gmail.com>
2020-09-03 06:30:59 +0200

    chore: organizacija

---
+++ Assignments/S3R5/2/UPN - Nekrep/01/src/main.cpp
@@ -0,0 +1,13 @@
+// 01. Vaja Nejc Drobnic - Prvi Program (2UPN01) - To je komentar
+
+#include <cstdio>
+
+int main() {
+
+	// Tu vstavi poimenovane podatke
+	printf("Ime in Priimek\n");
+	printf("Naslov\n");
+	printf("Postna stevilka in Mesto\n");
+
+	return 0;
+}
